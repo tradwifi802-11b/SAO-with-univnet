4faa8bb7b36aebddc18fd8839c97bd952bc8a199
1lmao <smira009@ucr.edu>
2025-06-03 03:32:13 -0700

    SAO + UnivNet: 291 and 314 outputs & evals

--- sao.py
+++ sao.py
@@ -42,7 +42,7 @@ torchaudio.save("output.wav", output, sample_rate)
 
 process_file('output.wav')
 inference(
-    checkpoint_path="chkpt/wokerun1_0314.pt",
+    checkpoint_path="chkpt/wokerun1_0291.pt",
     input_folder="mel",
     output_folder="output",
     config_path="config/config.yaml"
